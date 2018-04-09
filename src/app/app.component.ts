2fd8d5579486ed00aa7f768b095d2d0b15b51cec
sathyapv92 <sathapv92@gmail.com>
2018-04-09 09:18:19 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  invoiceForm: FormGroup;
+  constructor(private _fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.invoiceForm = this._fb.group({
+      itemRows: this._fb.array([this.initItemRows()])
+    });
+  }
+
+  /*
+  This creates a new formgroup. You can think of it as adding an empty object
+  into an array. So we are pushing an object to the formarray 'itemrows' that
+  has the property 'itemname'. 
+  */
+  initItemRows() {
+      return this._fb.group({
+          itemname: ['']
+      });
+  }
+
+  addNewRow() {
+      const control = <FormArray>this.invoiceForm.controls['itemRows'];
+      control.push(this.initItemRows());
+  }
+
+  deleteRow(index: number) {
+      const control = <FormArray>this.invoiceForm.controls['itemRows'];
+      control.removeAt(index);
+  }
 }
