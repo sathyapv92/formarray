6fc8a2679a32031ccca2b9d665b638bb8c60ba01
sathyapv92 <sathapv92@gmail.com>
2018-04-09 09:45:47 +0530

    sec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { MatTableModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { TableSelectionExample } from './table-selection-example/table-selection-example.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableSelectionExample
   ],
   imports: [
-    BrowserModule,ReactiveFormsModule
+    BrowserModule,ReactiveFormsModule,BrowserAnimationsModule,NoopAnimationsModule,
+    MatTableModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
